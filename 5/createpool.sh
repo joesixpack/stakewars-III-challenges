0e47dfa34b2a842dbf882fc5a901cbf8a3800fc4
joesixpack <23145642+joesixpack@users.noreply.github.com>
2022-08-14 14:56:16 -0700

    Update createpool.sh

--- 5/createpool.sh
+++ 5/createpool.sh
@@ -6,7 +6,11 @@ AMOUNT=100
 
 source ~/.profile
 
-near call factory.shardnet.near create_staking_pool '{"staking_pool_id": "$NEAR_POOL", "owner_id": "$NEAR_WALLET", "stake_public_key": "fromvalidatorkey", "reward_fee_fraction": {"numerator": 5, "denominator": 100}, "code_hash":"DD428g9eqLL8fWUxv8QSpVFzyHi1Qd16P8ephYCTmMSZ"}' --accountId $NEAR_WALLET --amount 30 --gas $NEAR_GAS --node_url $NEAR_NODE
+a="$(cat ~/.near/validator_key.json)"
+pat='\"public_key\":\"(.*)\",'
+[[ "$a" =~ $pat ]]; echo "${BASH_REMATCH[1]}"
+
+near call factory.shardnet.near create_staking_pool '{"staking_pool_id": "$NEAR_POOL", "owner_id": "$NEAR_WALLET", "stake_public_key": "${BASH_REMATCH[1]}", "reward_fee_fraction": {"numerator": 5, "denominator": 100}, "code_hash":"DD428g9eqLL8fWUxv8QSpVFzyHi1Qd16P8ephYCTmMSZ"}' --accountId $NEAR_WALLET --amount 30 --gas $NEAR_GAS --node_url $NEAR_NODE
 
 sleep 60
 
