6dd6ddcb055170f02fb417a6179e1b7d67a325e8
joesixpack <23145642+joesixpack@users.noreply.github.com>
2022-08-15 10:40:01 -0700

    Update createpool.sh

--- 5/createpool.sh
+++ 5/createpool.sh
@@ -10,8 +10,8 @@ a="$(cat ~/.near/validator_key.json)"
 pat='\"public_key\":\"(.*)\",'
 [[ "$a" =~ $pat ]]; echo "${BASH_REMATCH[1]}"
 
-near call factory.shardnet.near create_staking_pool '{"staking_pool_id": "$NEAR_POOL", "owner_id": "$NEAR_WALLET", "stake_public_key": "${BASH_REMATCH[1]}", "reward_fee_fraction": {"numerator": 5, "denominator": 100}, "code_hash":"DD428g9eqLL8fWUxv8QSpVFzyHi1Qd16P8ephYCTmMSZ"}' --accountId $NEAR_WALLET --amount 30 --gas $NEAR_GAS --node_url $NEAR_NODE
+near call factory.shardnet.near create_staking_pool '{"staking_pool_id": "$NEAR_POOL", "owner_id": "$NEAR_WALLET", "stake_public_key": "${BASH_REMATCH[1]}", "reward_fee_fraction": {"numerator": 5, "denominator": 100}, "code_hash":"DD428g9eqLL8fWUxv8QSpVFzyHi1Qd16P8ephYCTmMSZ"}' --accountId $NEAR_WALLET --amount 30 --gas $NEAR_GAS
 
 sleep 60
 
-near call $NEAR_POOL deposit_and_stake --account_id $NEAR_WALLET --amount $AMOUNT --gas $NEAR_GAS --node_url $NEAR_NODE
+near call $NEAR_POOL deposit_and_stake --account_id $NEAR_WALLET --amount $AMOUNT --gas $NEAR_GAS
